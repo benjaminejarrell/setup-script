aa2accc2649a5e9924b0849786ac9361d23e9861
Benjamin Jarrell <benjaminejarrell@gmail.com>
2025-01-06 15:21:33 -0600

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,4 +1,4 @@
-$chocoPackages = "terraform-docs", "act-cli", "git", "vscode", "awscli", "nodejs"
+$chocoPackages = "terraform-docs", "act-cli", "git", "vscode", "awscli", "nodejs", "sql-server-management-studio"
 Install-Module -Name ExchangeOnlineManagement -Confirm
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
