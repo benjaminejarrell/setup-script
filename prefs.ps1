c559cea475828deac6baad5cb8e847095c34d3de
Benjamin Jarrell <benjaminejarrell@gmail.com>
2025-01-08 16:28:46 -0600

    add explorer

--- prefs.ps1
+++ prefs.ps1
@@ -2,3 +2,9 @@ git config --global user.email "benjaminejarrell@gmail.com"
 git config --global user.name "Benjamin Jarrell"
 
 git config --global push.autoSetupRemote true
+
+# File Extensions
+Set-Itemproperty -path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'HideFileExt' -value 0
+
+# Item checkboxes
+Set-Itemproperty -path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'AutoCheckSelect' -value 0
