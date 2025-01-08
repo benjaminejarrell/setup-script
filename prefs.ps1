557787305559b2c19228bac9bb4eb38745504df7
Benjamin Jarrell <benjaminejarrell@gmail.com>
2025-01-08 16:17:57 -0600

    add prefs

---
+++ prefs.ps1
@@ -0,0 +1,4 @@
+git config --global user.email "benjaminejarrell@gmail.com"
+git config --global user.name "Benjamin Jarrell"
+
+git config --global push.autoSetupRemote true
--- system-install.ps1
+++ system-install.ps1
@@ -1,7 +1,14 @@
-$chocoPackages = "terraform-docs", "act-cli", "git", "vscode", "awscli", "nodejs", "sql-server-management-studio"
+$chocoPackages = @(
+    "terraform"
+    "terraform-docs"
+    "act-cli"
+    "git"
+    "vscode"
+    "awscli"
+    "nodejs"
+    "sql-server-management-studio"
+)
 Install-Module -Name ExchangeOnlineManagement -Confirm
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 $chocoPackages | % {choco install $_ -y}
-
-git config --global push.autoSetupRemote true
